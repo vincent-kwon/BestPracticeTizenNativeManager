e80b9f8e4b05a36f4d91ffbb96e733f9676a9157
Hobum (Vincent) Kwon <hobum.kwon@gmail.com>
2020-08-28 00:50:41 +0900

    Add dbus tizen usage.

--- src/bp_manager_tizen_implementation.c
+++ src/bp_manager_tizen_implementation.c
@@ -52,3 +52,56 @@ int main()
 
 	return 0;
 }
+
+#if 0
+	alarm_context.proxy = g_dbus_proxy_new_sync(alarm_context.connection,
+			G_DBUS_PROXY_FLAGS_DO_NOT_AUTO_START_AT_CONSTRUCTION,
+			NULL,
+			"org.tizen.alarm.manager",
+			"/org/tizen/alarm/manager",
+			"org.tizen.alarm.manager",
+			NULL,
+			NULL);
+
+	if (alarm_context.proxy == NULL) {
+		LOGE("Creating a proxy is failed.");
+		g_object_unref(alarm_context.connection);
+		return ERR_ALARM_SYSTEM_FAIL;
+	}
+
+	sub_initialized = true;
+
+	return ALARMMGR_RESULT_SUCCESS;
+
+		return_code = __dbus_call_sync(context.proxy, "alarm_create_noti",
+			param, &reply);
+	if (return_code != ALARMMGR_RESULT_SUCCESS) {
+		if (error_code)
+			*error_code = return_code;
+		return false;
+	}
+
+	static int __dbus_call_sync(GDBusProxy *proxy, const gchar *method_name,
+		GVariant *param, GVariant **reply)
+{
+	int error_code = ALARMMGR_RESULT_SUCCESS;
+	GError *error = NULL;
+
+	*reply = g_dbus_proxy_call_sync(proxy, method_name, param,
+			G_DBUS_CALL_FLAGS_NONE, -1, NULL, &error);
+	if (error) {
+		if (error->code == G_DBUS_ERROR_ACCESS_DENIED)
+			error_code = ERR_ALARM_NO_PERMISSION;
+		else
+			error_code = ERR_ALARM_SYSTEM_FAIL;
+
+		LOGE("%s : g_dbus_proxy_call_sync() failed.\
+				error_code[%d]. error->message is %s(%d)",
+				method_name, error_code, error->message, error->code);
+
+		g_error_free(error);
+	}
+
+	return error_code;
+}
+#endif	
